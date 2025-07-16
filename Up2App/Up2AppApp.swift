a5211267b72ea276f67e18ba3c13640e9afe06e0
jayraj <jayraj0908@gmail.com>
2025-07-15 21:41:11 -0700

    Initial Commit

---
+++ Up2App/Up2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Up2AppApp.swift
+//  Up2App
+//
+//  Created by jayraj patel on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Up2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Up2AppTests/Up2AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Up2AppTests.swift
+//  Up2AppTests
+//
+//  Created by jayraj patel on 15/07/25.
+//
+
+import Testing
+@testable import Up2App
+
+struct Up2AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
